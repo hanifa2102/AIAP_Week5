2ae1315364812538f237361001f397c9645cd5f3
Hanifa <hanifa.email@gmail.com>
2018-12-12 09:28:41 +0000

    Server Commit

---
+++ setup.sh
@@ -0,0 +1,7 @@
+wget http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz
+
+rm -rf aclImdb/train/unsup/1*
+rm -rf aclImdb/train/unsup/2*
+rm -rf aclImdb/train/unsup/3*
+rm -rf aclImdb/train/unsup/4*
+rm -rf aclImdb/train/unsup/5*
