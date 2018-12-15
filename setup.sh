c43321093e2a3d6a6a0a59bda55dacebf7f44c45
Hanifa <hanifa.email@gmail.com>
2018-12-15 17:34:28 +0000

    chapter1andchapter3Done

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 wget http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz
 tar -xzf aclImdb_v1.tar.gz
 
-rm -rf aclImdb/train/unsup/1*
-rm -rf aclImdb/train/unsup/2*
-rm -rf aclImdb/train/unsup/3*
-rm -rf aclImdb/train/unsup/4*
-rm -rf aclImdb/train/unsup/5*
-
 mv aclImdb/train/unsup aclImdb/
 mv aclImdb/train/*unsup* aclImdb/
+
+rm -rf aclImdb/unsup/1*
+rm -rf aclImdb/unsup/2*
+rm -rf aclImdb/unsup/3*
+rm -rf aclImdb/unsup/4*
+rm -rf aclImdb/unsup/5*
+
+
